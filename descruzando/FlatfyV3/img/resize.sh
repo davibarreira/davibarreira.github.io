a8c748bdbbb54761b9465a97d4d78a412446215f
davibarreira <davisbarreira@gmail.com>
2020-10-05 17:43:30 -0300

    Reformulando site com Jekyll

--- descruzando/FlatfyV3/img/resize.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-SRC="$1"
-convert $SRC -resize 1200 res-$SRC
-# To use this script,
-# run the following from a terminal
-# in a folder containing an image named foo.png (or whatever):
-#   chmod u+x convert.sh
-#   ./convert.sh foo
