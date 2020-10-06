b49bd444497158f32c2bb4e48ed364ddc6168702
davibarreira <davisbarreira@gmail.com>
2020-10-06 11:25:31 -0300

    Testando post para importacao

---
+++ _posts/2020-10-06-post-standard.md
@@ -0,0 +1,15 @@
+---
+layout: posts
+title: "Post Import Medium Test"
+---
+
+The preferred way of using images is placing them in the `/assets/images/` directory and referencing them with an absolute path. Prepending the filename with `{% raw %}{{ site.url }}{{ site.baseurl }}/assets/images/{% endraw %}` will make sure your images display properly in feeds and such.
+
+$$
+x = y ^2
+$$
+
+Standard image with no width modifier classes applied.
+
+**HTML:**
+
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": ["image","Post Formats"],
         "url": "http://localhost:4000/post%20formats/post-image-standard/",
         "teaser": null
+      },{
+        "title": "Post Import Medium Test",
+        "excerpt":"The preferred way of using images is placing them in the /assets/images/ directory and referencing them with an absolute path. Prepending the filename with {{ site.url }}{{ site.baseurl }}/assets/images/ will make sure your images display properly in feeds and such. \\[x = y ^2\\] Standard image with no width modifier...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/post-standard/",
+        "teaser": null
       }]
