ce147c80927e01ef7f676113dfc657ff87e3bd8f
davibarreira <davisbarreira@gmail.com>
2020-10-06 11:15:52 -0300

    MathJax funcionando

--- _posts/2010-08-05-post-image-standard.md
+++ _posts/2010-08-05-post-image-standard.md
@@ -1,4 +1,5 @@
 ---
+layout: posts
 title: "Post: Image (Standard)"
 categories:
   - Post Formats
@@ -8,6 +9,10 @@ tags:
 ---
 
 The preferred way of using images is placing them in the `/assets/images/` directory and referencing them with an absolute path. Prepending the filename with `{% raw %}{{ site.url }}{{ site.baseurl }}/assets/images/{% endraw %}` will make sure your images display properly in feeds and such.
+
+$$
+x = y ^2
+$$
 
 Standard image with no width modifier classes applied.
 
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Post: Image (Standard)",
+        "excerpt":"The preferred way of using images is placing them in the /assets/images/ directory and referencing them with an absolute path. Prepending the filename with {{ site.url }}{{ site.baseurl }}/assets/images/ will make sure your images display properly in feeds and such. \\[x = y ^2\\] Standard image with no width modifier...","categories": ["Post Formats"],
+        "tags": ["image","Post Formats"],
+        "url": "http://localhost:4000/post%20formats/post-image-standard/",
+        "teaser": null
+      }]
